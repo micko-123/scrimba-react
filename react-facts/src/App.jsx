71d3017e3a70d9bf3a6387bf0fd4a93193635a1f
Mickyas Asnake <60357226+micko-123@users.noreply.github.com>
2022-11-29 11:06:16 -0800

    react fact initial

---
+++ react-facts/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return ( 
+      <nav>
+      <img src={viteLogo}/>
+      <img src={reactLogo}/>
+      <h1>fun facts about react</h1>
+      <ul>
+        <li>was first released in 2013</li>
+        <li>was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by FaceBook</li>
+      </ul>
+      </nav>
+  )
+  
+}
+
+export default App
---
+++ react-facts/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import viteLogo from '../public/vite.svg'
+import reactLogo from './assets/react.svg'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
