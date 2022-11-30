b4957ede1ed858eacf62c578cf917e303330e1dc
Mickyas Asnake <60357226+micko-123@users.noreply.github.com>
2022-11-29 19:00:02 -0800

    custom component

--- react-facts/src/App.jsx
+++ react-facts/src/App.jsx
@@ -1,23 +1,63 @@
 import { useState } from 'react'
 import viteLogo from '../public/vite.svg'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/react.svg' 
 
+
+function Header(){
+  return(
+    
+      <nav className='nav'>
+        <img src={viteLogo} />
+        <img src={reactLogo} />
+        
+        <ui className = 'nav-list'>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ui>
+      </nav>
+  )
+}
+
+function MainContent(){
+
+  return(
+  <div>
+    <h1>fun facts about react</h1>
+     <ul>
+         <li>was first released in 2013</li>
+         <li>was originally created by Jordan Walke</li>
+         <li>Has well over 100K stars on GitHub</li>
+         <li>Is maintained by FaceBook</li>
+     </ul>
+  </div>
+  )
+}
+
+function Footer(){
+  return(
+    <footer>
+    <small>2022 Mickyas, all rights reserved ! </small>
+   </footer>
+  )
+}
+function MyReasons() {
+  return(
+    <>
+    <Header />
+    <MainContent />
+    <Footer />
+    </>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return ( 
-      <nav>
-      <img src={viteLogo}/>
-      <img src={reactLogo}/>
-      <h1>fun facts about react</h1>
-      <ul>
-        <li>was first released in 2013</li>
-        <li>was originally created by Jordan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by FaceBook</li>
-      </ul>
-      </nav>
+            
+      <MyReasons />
+  
   )
   
 }
